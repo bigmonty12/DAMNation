d31ffdb44569895cda0ad2bd0a1823660e439038
Austin Montgomery <austinbrucemontgomery@gmail.com>
2020-01-23 11:15:28 -0700

    Create repo for DAM shiny app

---
+++ DAM-functions.R
@@ -0,0 +1,127 @@
+
+# Convert raw data to binary sleep data
+find.sleep <- function(df) {
+  new <- rep(NA, length(df))
+  for (i in seq(from = 3, to = length(df) - 2)) {
+    if (sum(df[i:(i+2)]) == 3) {
+      new[c(i:(i+2))] = 1
+    } else if (sum(df[i:(i-2)]) == 3) {
+      new[c(i:(i-2))] = 1
+    } else{
+      new[[i]] = 0
+    }
+  }
+  new[is.na(new)] <- 0
+  return(new)
+}
+
+# Find dead flies (no activity for last 2 hours and no activity after experiment ends)
+find.death.before <- function(df) {
+  return(sum(df %in% 60, na.rm = T) == 2)
+}
+
+find.death.after <- function(df) {
+  return(sum(df) == 0)
+}
+
+# Find latency (how long it takes to fall asleep) from sleep data
+get.latency = function(df, dates, days) {
+  nights <- df[hour(df[[1]]) >= 12,]
+  results <- data.frame(matrix(nrow = days, ncol = length(df) - 1))
+  results[1] <- dates
+  for (j in seq_len(days)) {
+    night <- nights[date(nights[[1]]) == dates[j], ]
+    loc = 2
+    for (col in night[3:length(night)]) {
+      count = 0
+      for (i in seq_along(col)) {
+        if (col[i] == 0) {
+          count <- count + 1
+        } else {
+          results[j, loc] <- count * 2
+          loc <- loc + 1
+          break
+        }
+      }
+      if (count == 360) {
+        results[j, loc] <- count * 2
+        loc <- loc + 1
+      }
+    }
+  }
+  return(results)
+}
+
+# Find the time of each shake and the previous measuring period
+# x is number of days in experiment
+find.each_shake <- function(x) {
+  nums <- c()
+  for (i in seq_len(x) - 1) {
+    a = 1:2 + i * 30
+    nums <- c(nums, a)
+  }
+  return(nums)
+}
+
+# Find if flies woke up when nutated
+find.arousal <- function(df, deadf, dates, days, num_shakes) {
+  results <- data.frame(matrix(nrow = days * num_shakes, ncol = length(df) - 1))
+  results[1] <- rep(dates, num_shakes)
+  iloc = 1
+  for (col in df[3:length(df)]) {
+    iloc <- iloc + 1
+    loc <- 0
+    for (i in seq(1, length(col), by = 2)) {
+      loc <- loc + 1
+      day = col[i:(i+1)]
+      if (day[1] == 0) {
+        results[loc, iloc] <- 0
+      } else {
+        if (day[2] == 1) {
+          results[loc, iloc] <- 1
+        } else {
+          results[loc, iloc] <- 2
+        }
+      }
+    }
+  }
+  iloc <- iloc + 1
+  if (deadf > 0) {
+  dead_results <- results[,-c(deadf)]
+  } else {
+    dead_results <- results 
+  }
+  for (i in seq_len(days * num_shakes)) {
+    r <- length(which(dead_results[i,] == 2))
+    w <- length(which(dead_results[i,] == 1))
+    t <- r + w
+    results[i, iloc] <- paste0(r, sep='/', t, ' (woke up/were asleep)')
+  }
+  return(results[order(results[1]),])
+}
+
+# Find bouts: when and how long
+find.bouts <- function(df) {
+  bouts_list <- list()
+  index = 1
+  for (i in seq(3, length(df))) {
+    runs <- rle(df[[i]])
+    myruns <- which(runs$values == 1)
+    runs.lengths.cumsum = cumsum(runs$lengths)
+    ends = runs.lengths.cumsum[myruns]
+    newindex = ifelse(myruns > 1, myruns - 1, 0)
+    starts = runs.lengths.cumsum[newindex] + 1
+    if (0 %in% newindex) starts = c(1,starts)
+    width <- (ends - starts + 1) * 2
+    start_time <- df[[1]][starts]
+    #print(hour(start_time))
+    end_time <- df[[1]][ends + 1]
+    period <- ifelse(hour(start_time) < 12, "Light", "Dark") 
+    bouts <- data.frame(idx = seq_along(start_time), start = start_time, end = end_time, length = width, period = period)
+    bouts_list[[index]] <- bouts
+    colnames(bouts_list[[index]]) <- c("idx", paste0("start_", index), paste0("end_", index), paste0("length_", index),
+                                       paste0("period_", index))
+    index <- index + 1
+  }
+  return(bouts_list)
+}
---
+++ settings_ui.R
@@ -0,0 +1,153 @@
+# settings.R
+# Home page of Shiny app
+
+fluidPage(
+  fluidRow(
+    column(12,
+           tags$h1("DAM Analysis", align = "center"),
+           tags$h3("Program analyzing locomotor activity in Trikinetics DAM system",
+                   align = "center"),
+           tags$h4("Austin Montgomery, Rothenfluh Lab, University of Utah",
+                   align = "center"),
+           
+           fileInput('file1', 'Upload Monitor Files', multiple = TRUE,
+                     accept = c('text/csv',
+                                'text/comma-separated-values,text/plain',
+                                '.csv')
+           )
+    )
+  ),
+  
+  wellPanel(
+    fluidRow(
+      column(2,
+             dateInput('dateStart',
+                            label = 'Start Date',
+                            format = "d M yy",
+                            value = Sys.Date()-10)),
+      column(2,
+             numericInput('experimentLength',
+                          'Number of Days',
+                          value = 3,
+                          min = 1,
+                          max = NA,
+                          step = 1)),
+      column(3,
+             numericInput("data_recording_frequency", 
+                          "DAM system data bins [min]",
+                          value = 2, 
+                          min = 1, 
+                          max = NA, 
+                          step = 1, 
+                          width = NULL)),
+      column(3,
+             selectInput("light_onset_time", "Light onset time",
+                         c("00:00","01:00","02:00","03:00","04:00","05:00","06:00", 
+                           "07:00", "08:00","09:00","10:00","11:00","12:00","13:00",
+                           "14:00","15:00", "16:00","17:00","18:00","19:00","20:00",
+                           "21:00","22:00","23:00"), 
+                         selected = "08:00", 
+                         multiple = FALSE,
+                         selectize = TRUE, 
+                         width = NULL, 
+                         size = NULL)),
+      column(2,
+             numericInput('monitorNumber',
+                          'Monitor Number',
+                          value = 1,
+                          min = 1,
+                          max = NA,
+                          step = 1))
+    )
+  ),
+  
+  fluidRow(
+    column(2,
+           tags$head(
+             tags$style(HTML('#go{background-color:#67EB5E}'))),
+           actionButton("go", "Start Analysis"))),
+    # column(1,
+    #        tags$head(
+    #          tags$style(HTML('#reset{background-color:#F22E2E'))
+    #        ),
+    #        actionButton("reset", "Reset"))),
+  
+  # ==== Output Plots ====
+  conditionalPanel(
+    condition = "input.go != 0",
+    
+    wellPanel(
+      fluidRow(
+        column(7,
+               tags$h3("DAM Summary"))),
+      fluidRow(
+        column(12,
+               tableOutput('summary'))
+      ),
+      fluidRow(
+        column(3,
+               downloadButton("downloadSummary", "Download DAM-Summary"))
+      )
+      ),
+    wellPanel(
+      fluidRow(
+        column(7,
+               tags$h3("DAM Bouts"))),
+      fluidRow(
+        column(12,
+               tableOutput('bouts'))
+      ),
+      fluidRow(
+        column(3,
+               downloadButton("downloadBouts", "Download DAM-Bouts"))
+      )
+    ),
+    wellPanel(
+      fluidRow(
+        column(7,
+               tags$h3("DAM Average Sleep"))),
+      fluidRow(
+        column(12,
+               tableOutput('ave_sleep'))
+      ),
+      fluidRow(
+        column(3,
+               downloadButton("downloadAveSleep", "Download DAM-Average-Sleep"))
+      )
+      ),
+    wellPanel(
+      fluidRow(
+        column(7,
+               tags$h3("Sleep-o-gram"))
+      ),
+      fluidRow(
+        column(12,
+               plotOutput('ave_agg_sleep'))
+      ),
+      fluidRow(
+        column(3,
+               downloadButton("downloadSleepGram", "Download Sleep-o-gram")),
+        column(3,
+               downloadButton("downloadSleepGramData", "Download Sleep-o-gram Data"))
+      )
+    ),
+    wellPanel(
+      fluidRow(
+        column(7,
+               tags$h3("Act-o-gram"))
+      ),
+      fluidRow(
+        column(12,
+               plotOutput('activity_graph'))
+      ),
+      fluidRow(
+        column(3,
+               downloadButton("downloadActGram", "Download Act-o-gram")),
+        column(3,
+               downloadButton("downloadActGramData", "Download Act-o-gram Data"))
+      )
+    )
+    )
+)
+
+  
